defad8c2c818de05a34adf4e57ed0a6950968195
Malaka-KG <darkalfhabot@gmail.com>
2024-12-09 13:48:32 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,27 @@
+ const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "VAJIRA-MD=LIll0A4J#lt0Brr6GJ8TMK6B0WrYR6cYSqHUjP2hy6Y4M3Xo8j5k",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
+    MODE: process.env.MODE || "public",
+    AUTO_VOICE: process.env.AUTO_VOICE || "true",
+    AUTO_STICKER: process.env.AUTO_STICKER || "false",
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://pomf2.lain.la/f/fxhw0z8c.jpg",
+    ALIVE_MSG: process.env.ALIVE_MSG || "",
+    ANTI_LINK: process.env.ANTI_LINK || "false",
+    ANTI_BAD: process.env.ANTI_BAD || "true",
+    PREFIX: process.env.PREFIX || ".",
+    FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    HEART_REACT: process.env.HEART_REACT || "true",
+    OWNER_REACT: process.env.OWNER_REACT || "true",
+    BOT_NAME: process.env.BOT_NAME || "*👿☠️_KD_PANTA_00_☠️👿*",
+    OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+};
