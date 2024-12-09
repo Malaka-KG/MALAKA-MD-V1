d4feafbd39b675a115158edbecd1eb9acf8d3b72
Malaka-KG <darkalfhabot@gmail.com>
2024-12-09 22:01:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "VAJIRA-MD=LIll0A4J#lt0Brr6GJ8TMK6B0WrYR6cYSqHUjP2hy6Y4M3Xo8j5k",
+    SESSION_ID: process.env.SESSION_ID || "youre session id",
     MONGODB: process.env.MONGODB || "enter mongodb here",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/QNwLWTN/20241201-230018.jpg",
+    PREFIX: process.env.PREFIX || ".",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
