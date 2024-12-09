dba457bfe551fab5ad9cc27c30c2c8be22f76f35
Malaka-KG <darkalfhabot@gmail.com>
2024-12-09 21:32:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,14 +8,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "VAJIRA-MD=LIll0A4J#lt0Brr6GJ8TMK6B0WrYR6cYSqHUjP2hy6Y4M3Xo8j5k",
     MONGODB: process.env.MONGODB || "enter mongodb here",
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-    MODE: process.env.MODE || "public",
-    AUTO_VOICE: process.env.AUTO_VOICE || "true",
-    AUTO_STICKER: process.env.AUTO_STICKER || "false",
-    AUTO_REPLY: process.env.AUTO_REPLY || "false",
-    PREFIX: process.env.PREFIX || ".",
-    AUTO_REACT: process.env.AUTO_REACT || "false",
-    HEART_REACT: process.env.HEART_REACT || "true",
-    OWNER_REACT: process.env.OWNER_REACT || "true",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
