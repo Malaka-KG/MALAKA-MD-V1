d71d7968ecf41a2529aafcc0043aadbf3960ac1d
Malaka-KG <darkalfhabot@gmail.com>
2024-12-10 22:54:11 +0530

    Update MALAKA-menu.js

--- plugins/MALAKA-menu.js
+++ plugins/MALAKA-menu.js
@@ -1,5 +1,5 @@
 const config = require('../config')
-const {cmd , commands} = require('../command')
+const {cmd , commands} = require('../lib/command')
 const os = require("os")
 const {runtime} = require('../lib/functions')
 const axios = require('axios')
