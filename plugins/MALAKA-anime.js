b14c41b4c8591fb2bd76fc03794476eafdca92e4
Malaka-KG <darkalfhabot@gmail.com>
2024-12-10 22:51:59 +0530

    Update MALAKA-anime.js

--- plugins/MALAKA-anime.js
+++ plugins/MALAKA-anime.js
@@ -1,4 +1,4 @@
-const { cmd, commands } = require('../command')
+const { cmd, commands } = require('../lib/command')
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
 const fs = require('fs');
 const axios = require('axios')
