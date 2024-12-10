64662caa00603e0e2090d8c34f8f5d3a0af9bf25
Malaka-KG <darkalfhabot@gmail.com>
2024-12-11 00:21:45 +0530

    Update MALAKA-ai.js

--- plugins/MALAKA-ai.js
+++ plugins/MALAKA-ai.js
@@ -2,60 +2,18 @@ const config = require('../config')
 const {cmd , commands} = require('../lib/command')
 const { fetchJson } = require('../lib/functions')
 
-const config = {
-  pattern: "bard",
-  alias: ["bardai", "gbard", "googlebard", "googleai", "ai"],
-  react: '👾',
-  desc: desct,
-  category: "search",
-  use: ".bard ha",
-  filename: __filename
-};
-
-cmd(config, async (context, args, utils, {
-  from,
-  l,
-  prefix,
-  quoted,
-  body,
-  isCmd,
-  command,
-  args: cmdArgs,
-  q,
-  isGroup,
-  sender,
-  senderNumber,
-  botNumber2,
-  botNumber,
-  pushname,
-  isMe,
-  isOwner,
-  groupMetadata,
-  groupName,
-  participants,
-  groupAdmins,
-  isBotAdmins,
-  isAdmins,
-  reply
-}) => {
-  try {
-    if (!q) {
-      return reply(needus);
-    }
-    
-    let apiData = await fetchJson("https://gist.githubusercontent.com/vihangayt0/7dbb65f6adfe21538f7febd13982569a/raw/apilis.json");
-    let users = apiData.users;
-    let randomUser = users[Math.floor(Math.random() * users.length)];
-    
-    const response = await fetchJson(`${apiData.xz}api/bard?text=${q}&apikey=${randomUser}`);
-    return await reply(response.content);
-  } catch (error) {
-    try {
-      const fallbackResponse = await fetchJson(`https://api.akuari.my.id/ai/gbard?chat=${q}`);
-      return await reply(fallbackResponse.respon);
-    } catch (fallbackError) {
-      reply(cantf);
-      l(fallbackError);
-    }
-  }
-});
+cmd({
+    pattern: "ai",
+    desc: "ai chat",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let data = await fetchJson(`https://api.akuari.my.id/ai/gbard?chat=${q}`)
+return reply(`${data.data}`)
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
