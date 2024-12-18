ad902bc949f45a915c91dc11c6c10dda44686e98
Malaka-KG <darkalfhabot@gmail.com>
2024-12-18 13:39:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ const prefix = '.'
 const ownerNumber = ['94704243771']
 
 //===================SESSION-AUTH============================
-if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
+if (!fs.existsSync(__dirname + '/session/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
 const sessdata = config.SESSION_ID
 const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
 if(err) throw err
-fs.writeFile(__dirname + '/auth_info_baileys/creds.json', data, () => {
+fs.writeFile(__dirname + '/session/creds.json', data, () => {
 console.log("Session downloaded ✅")
 })})}
 
@@ -49,7 +49,7 @@ const config = await readEnv();
 //==============================================
         
 console.log("Connecting Darkalfha bot 😻...");
-const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
+const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/session/')
 var { version } = await fetchLatestBaileysVersion()
 
 const conn = makeWASocket({
