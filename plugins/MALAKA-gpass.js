1bea402ce31a043ac7f3f0561796320b9e649f19
Malaka-KG <darkalfhabot@gmail.com>
2024-12-20 21:38:47 +0530

    Update MALAKA-gpass.js

--- plugins/MALAKA-gpass.js
+++ plugins/MALAKA-gpass.js
@@ -1,76 +1,122 @@
-const {readEnv} = require('../lib/database')
-const { cmd } = require('../command')
-const fs = require('fs');
-const path = require('path');
-// List of bad words to check against
- // Replace with actual words
+const {
+  cmd,
+  commands
+} = require("../command");
+const {
+  readEnv
+} = require('../lib/database');
 
 cmd({
-  on: "body"
-},
-async (conn,mek, m, { from, body, isGroup, isAdmins, isBotAdmins, reply, sender }) => {
-    try {
-    
-        const badWords = ["wtf", "mia", "xxx","fuck","sex","huththa","pakaya","ponnaya","hutto"]
-        if (!isGroup || isAdmins || !isBotAdmins) return; // Skip if not in group, or sender is admin, or bot is not admin
-      
-        const lowerCaseMessage = body.toLowerCase();
-        const containsBadWord = badWords.some(word => lowerCaseMessage.includes(word));
-        
-        if (containsBadWord & config.ANTI_BAD_WORD === 'true') {
-          await conn.sendMessage(from, { delete: mek.key }, { quoted: mek });
-          await conn.sendMessage(from, { text: "🚫 ⚠️BAD WORDS NOT ALLOWED⚠️ 🚫" }, { quoted: mek });
-        }
-    } catch (error) {
-        console.error(error)
-        reply("An error occurred while processing the message.")
+  'on': 'body'
+}, async (message, quotedMessage, key, {
+  from: fromId,
+  body: messageBody,
+  isGroup,
+  isAdmins,
+  isBotAdmins,
+  reply,
+  sender
+}) => {
+  try {
+    const badWords = ['wtf', "mia", "xxx", 'fuck', "sex", 'huththa', "pakaya", "ponnaya", 'hutto', 'lol'];
+    if (!isGroup || isAdmins || !isBotAdmins) {
+      return;
     }
-})
+    const lowerCaseMessageBody = messageBody.toLowerCase();
+    const containsBadWord = badWords.some(word => lowerCaseMessageBody.includes(word));
+    const envConfig = await readEnv();
+    if (containsBadWord && envConfig.ANTI_BAD === "true") {
+      await message.sendMessage(fromId, {
+        'delete': quotedMessage.key
+      }, {
+        'quoted': quotedMessage
+      });
+      await message.sendMessage(fromId, {
+        'text': "*🚫 ⚠️BAD WORDS NOT ALLOWED⚠️ 🚫*",
+        'contextInfo': {
+          'mentionedJid': ["94779062397@s.whatsapp.net"],
+          'groupMentions': [],
+          'forwardingScore': 1,
+          'isForwarded': true,
+          'forwardedNewsletterMessageInfo': {
+            'newsletterJid': "120363192254044294@newsletter",
+            'newsletterName': "Lααɾα-ᴍᴅ ✻",
+            'serverMessageId': 999
+          },
+          'externalAdReply': {
+            'title': "LARA MD",
+            'body': "ꜱᴀᴅᴇᴇꜱʜᴀ ᴛʜᴀʀᴜᴍɪɴ",
+            'mediaType': 1,
+            'sourceUrl': 'https://github.com/sadiyamin',
+            'thumbnailUrl': 'https://raw.githubusercontent.com/tharumin/Alexa_Voice/refs/heads/main/20241214_204755.jpg',
+            'renderLargerThumbnail': false,
+            'showAdAttribution': true
+          }
+        }
+      }, {
+        'quoted': quotedMessage
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    reply("An error occurred while processing the message.");
+  }
+});
 
-
-const linkPatterns = [
-    /https?:\/\/(?:chat\.whatsapp\.com|wa\.me)\/\S+/gi,   // WhatsApp group or chat links
-    /wa\.me\/\S+/gi,                                      // wa.me links without https
-    /https?:\/\/(?:t\.me|telegram\.me)\/\S+/gi,           // Telegram links
-    /https?:\/\/(?:www\.)?youtube\.com\/\S+/gi,           // YouTube links
-    /https?:\/\/youtu\.be\/\S+/gi,                        // YouTube short links
-    /https?:\/\/(?:www\.)?facebook\.com\/\S+/gi,          // Facebook links
-    /https?:\/\/fb\.me\/\S+/gi,                           // Facebook short links
-    /https?:\/\/(?:www\.)?instagram\.com\/\S+/gi,         // Instagram links
-    /https?:\/\/(?:www\.)?twitter\.com\/\S+/gi,           // Twitter links
-    /https?:\/\/(?:www\.)?tiktok\.com\/\S+/gi,            // TikTok links
-    /https?:\/\/(?:www\.)?linkedin\.com\/\S+/gi,          // LinkedIn links
-    /https?:\/\/(?:www\.)?snapchat\.com\/\S+/gi,          // Snapchat links
-    /https?:\/\/(?:www\.)?pinterest\.com\/\S+/gi,         // Pinterest links
-    /https?:\/\/(?:www\.)?reddit\.com\/\S+/gi,            // Reddit links
-    /https?:\/\/ngl\/\S+/gi,                              // NGL links
-    /https?:\/\/(?:www\.)?discord\.com\/\S+/gi,           // Discord links
-    /https?:\/\/(?:www\.)?twitch\.tv\/\S+/gi,             // Twitch links
-    /https?:\/\/(?:www\.)?vimeo\.com\/\S+/gi,             // Vimeo links
-    /https?:\/\/(?:www\.)?dailymotion\.com\/\S+/gi,       // Dailymotion links
-    /https?:\/\/(?:www\.)?medium\.com\/\S+/gi             // Medium links
-];
+const linkPatterns = [/https?:\/\/(?:chat\.whatsapp\.com|wa\.me)\/\S+/gi, /^https?:\/\/(www\.)?whatsapp\.com\/channel\/([a-zA-Z0-9_-]+)$/];
 
 cmd({
-    on: "body"
-}, async (conn, mek, m, { from, body, sender, isGroup, isAdmins, isBotAdmins, reply }) => {
-    try {
-        if (!isGroup || isAdmins || !isBotAdmins) return; // Skip if not in group, or sender is admin, or bot is not admin
-
-        const containsLink = linkPatterns.some(pattern => pattern.test(body));
-
-        if (containsLink && config.ANTI_LINK === 'true') {
-            // Delete the message
-            await conn.sendMessage(from, { delete: mek.key }, { quoted: mek });
-
-            // Warn the user
-            await conn.sendMessage(from, { text: `⚠️ Links are not allowed in this group.\n@${sender.split('@')[0]} has been removed. 🚫`, mentions: [sender] }, { quoted: mek });
-
-            // Remove the user from the group
-            await conn.groupParticipantsUpdate(from, [sender], 'remove');
-        }
-    } catch (error) {
-        console.error(error);
-        reply("An error occurred while processing the message.");
+  'on': "body"
+}, async (message, quotedMessage, key, {
+  from: fromId,
+  body: messageBody,
+  sender,
+  isGroup,
+  isAdmins,
+  isBotAdmins,
+  reply
+}) => {
+  try {
+    if (!isGroup || isAdmins || !isBotAdmins) {
+      return;
     }
+    const containsLink = linkPatterns.some(pattern => pattern.test(messageBody));
+    const envConfig = await readEnv();
+    if (containsLink && envConfig.ANTI_LINK === 'true') {
+      await message.sendMessage(fromId, {
+        'delete': quotedMessage.key
+      }, {
+        'quoted': quotedMessage
+      });
+      await message.sendMessage(fromId, {
+        'text': "*⚠️ Links are not allowed in this group*\n@" + sender.split('@')[0] + " has been removed. 🚫",
+        'mentions': [sender],
+        'contextInfo': {
+          'mentionedJid': ["94779062397@s.whatsapp.net"],
+          'groupMentions': [],
+          'forwardingScore': 1,
+          'isForwarded': true,
+          'forwardedNewsletterMessageInfo': {
+            'newsletterJid': "120363192254044294@newsletter",
+            'newsletterName': "Lααɾα-ᴍᴅ ✻",
+            'serverMessageId': 999
+          },
+          'externalAdReply': {
+            'title': "MALAKA-MD",
+            'body': "ꜱᴀᴅᴇᴇꜱʜᴀ ᴛʜᴀʀᴜᴍɪɴ",
+            'mediaType': 1,
+            'sourceUrl': 'https://github.com/sadiyamin',
+            'thumbnailUrl': "https://raw.githubusercontent.com/tharumin/Alexa_Voice/refs/heads/main/20241214_204755.jpg",
+            'renderLargerThumbnail': false,
+            'showAdAttribution': true
+          }
+        }
+      }, {
+        'quoted': quotedMessage
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    reply("An error occurred while processing the message.");
+  }
 });
